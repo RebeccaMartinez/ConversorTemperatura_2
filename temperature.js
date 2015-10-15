bf4b5f32ef45ee1ccce7db03f09c65b7b2d23a22
Rebecca <rebe.mart.gal@gmai.com>
2015-10-15 20:05:31 +0100

    modificado el temperature.js

--- temperature.js
+++ temperature.js
@@ -27,35 +27,33 @@ function Temperatura(){
 Temperatura.prototype = new Medida();
 
 /*Pasa los Celsius a Fahrenheit y Kelvin*/
-Temperatura.prototype.celsius = function(num, result1, result2) {
-  result2 = num + 273.15;
-  result1 = (num * 9/5)+32;
+Temperatura.prototype.celsius = function() {
+  var result2 = this.get_valor() + 273.15;
+  var result1 = (this.get_valor() * 9/5)+32;
   result1 = result1.toFixed(1)+" Farenheit"
   result2 = result2.toFixed(1) + " Kelvin"
   return result1 + ", " +result2;
 };
 
 /*Pasa los Fahrenheit a Celsius y Kelvin*/
-Temperatura.prototype.fahrenheit = function(num, result1, result2) {
-  result1 = (num - 32)*5/9;
-  result2 = result1 + 273.15;
+Temperatura.prototype.fahrenheit = function() {
+  var result1 = (this.get_valor() - 32)*5/9;
+  var result2 = result1 + 273.15;
   result1 = result1.toFixed(1)+" Celsius"
   result2 = result2.toFixed(1) + " Kelvin"
   return result1 + ", " +result2;
 };
 
 /*Pasa los Kelvin a Celsius y Fahrenheit*/
-Temperatura.prototype.kelvin = function(num, result1, result2) {
-  result1 = num - 273.15;
-  result2 = 1.8 * (num - 273.15) + 32;
+Temperatura.prototype.kelvin = function() {
+  var result1 = this.get_valor() - 273.15;
+  var result2 = 1.8 * (this.get_valor() - 273.15) + 32;
   result1 = result1.toFixed(1)+" Celsius"
   result2 = result2.toFixed(1) + " Farenheit"
   return result1 + ", " +result2;
 };
 
 function calculate(){
-  var result1;
-  var result2;
   var final = 0;
   var temp = original.value;
   var regexp = /([+-]?\d+(?:\.\d*)?(?:\s*[eE]\d+)?)\s*([fFcCkK])/
@@ -67,13 +65,13 @@ function calculate(){
     convertir.set_tipo(m[2]);
     convertir.set_valor(m[1]);
     if(convertir.get_tipo() == 'c' || convertir.get_tipo() == 'C'){
-      final = convertir.celsius(m[1], result1, result2);
+      final = convertir.celsius();
     }
     if(convertir.get_tipo() == 'f' || convertir.get_tipo() == 'F'){
-      final = convertir.fahrenheit(m[1], result1, result2);
+      final = convertir.fahrenheit();
     }
     if(convertir.get_tipo() == 'k' || convertir.get_tipo() == 'K'){
-      final = convertir.kelvin(m[1], result1, result2);
+      final = convertir.kelvin();
     }
     document.getElementById("converted").setAttribute("col", "gr");
     converted.innerHTML = final;
